20020f0cdf91fef5a3750d35d45bc6ee112e4447
taller <leidyjohanasantacruzdaza03@gmail.com>
2024-08-13 21:29:56 -0500

    home

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,15 +8,18 @@ import { FormsModule } from '@angular/forms';
   imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
-
 })
 export class LoginComponent {
   username: string = '';
   password: string = '';
 
+  constructor(private router: Router) {}
+
   onSubmit() {
     console.log('Usuario:', this.username);
     console.log('Contraseña:', this.password);
     
+    // Redirigir al usuario a la vista de home después de iniciar sesión
+    this.router.navigate(['/home']);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(FormsModule)
+    provideRouter(routes)
   ]
-}).catch(err => console.error(err));
+});
